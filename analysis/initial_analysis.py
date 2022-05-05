c79bfc62c787a797be3ea255bcc838b84ac2a02c
Jesse Cusack <jmcusack@marine.rutgers.edu>
2022-05-05 17:16:54 -0400

    more analysis

--- analysis/initial_analysis.py
+++ analysis/initial_analysis.py
@@ -22,67 +22,93 @@ import numpy as np
 import matplotlib.pyplot as plt
 import utils
 import gsw
+import scipy.signal as sig
 
-ds = xr.open_dataset("../data/driftertrajGPS_1.03.nc")
-ds = ds.assign_coords({"BID": ds.ID})
+T_M2 = 24/12.4206012  # M2 tidal period in cycles per day
 
-# %%
+ds = xr.open_dataset("../data/internal/hourly_GPS_1.04.nc")
 ds
 
 # %%
-IDs = np.unique(ds.ID.values[np.isfinite(ds.ID.values)])
-fig, axs = plt.subplots(1, 2)
-axs[0].plot(IDs, '.')
-axs[1].hist(IDs[IDs > 5e7]);
-
-print(IDs[IDs > 5e7][::20])
+fig, ax = plt.subplots()
+ax.plot(ds.lon, ds.lat, '.', ms=0.2)
 
 # %%
-ds_ = ds.isel(TIME=ds.BID==64734970.)
+ds_ = ds.sel(ID = ds.ID_unique[100].data)
+
+print(ds_.time[0].data)
+print(ds_.time[-1].data)
+
 ds_
 
 # %%
-fcor = gsw.f(ds_.LAT.min())  # minimum coriolis
+fcor = gsw.f(ds_.lat.min())  # minimum coriolis
 Tcor = 2*np.pi/np.abs(fcor)
 Ts = 60.*60.  # Sampling period
-Tlpf = 1.1*Tcor  # Low pass period
+Tlpf = 2*Tcor  # Low pass period
 
-ul = utils.butter_filter(ds_.U, 1/Tlpf, 1/Ts)
-vl = utils.butter_filter(ds_.V, 1/Tlpf, 1/Ts)
+ul = utils.butter_filter(ds_.u, 1/Tlpf, 1/Ts)
+vl = utils.butter_filter(ds_.v, 1/Tlpf, 1/Ts)
 
 fig, ax = plt.subplots(1, 1)
-ax.plot(ds_.LON, ds_.LAT)
-ax.plot(ds_.LON[0], ds_.LAT[0], "go")
-ax.plot(ds_.LON[-1], ds_.LAT[-1], "ro")
+ax.plot(ds_.lon, ds_.lat)
+ax.plot(ds_.lon[0], ds_.lat[0], "go")
+ax.plot(ds_.lon[-1], ds_.lat[-1], "ro")
 
 fig, axs = plt.subplots(3, 1, sharex=True, figsize=(6, 9))
-axs[0].plot(ds_.TIME, ds_.U)
-axs[0].plot(ds_.TIME, ul)
-axs[1].plot(ds_.TIME, ds_.V)
-axs[1].plot(ds_.TIME, vl)
-axs[2].plot(ds_.TIME, ds_.U_ERR, ".")
+axs[0].plot(ds_.time, ds_.u)
+axs[0].plot(ds_.time, ul)
+axs[1].plot(ds_.time, ds_.v)
+axs[1].plot(ds_.time, vl)
+axs[2].plot(ds_.time, ds_.u_err, ".")
 
 fig.autofmt_xdate()
 
 # %%
 # zoom in on a chunk
-i0 = 100
-i1 = 500
+i0 = 3000
+i1 = i0 + 256
 
 cut = slice(i0, i1)
 
 fig, ax = plt.subplots(1, 1)
-ax.plot(ds_.LON[cut], ds_.LAT[cut])
-ax.plot(ds_.LON[cut][0], ds_.LAT[cut][0], "go")
-ax.plot(ds_.LON[cut][-1], ds_.LAT[cut][-1], "ro")
+ax.plot(ds_.lon[cut], ds_.lat[cut], ls="-", marker=".")
+ax.plot(ds_.lon[cut][0], ds_.lat[cut][0], "go")
+ax.plot(ds_.lon[cut][-1], ds_.lat[cut][-1], "ro")
 
 fig, axs = plt.subplots(3, 1, sharex=True, figsize=(6, 9))
-axs[0].plot(ds_.TIME[cut], ds_.U[cut])
-axs[0].plot(ds_.TIME[cut], ul[cut])
-axs[1].plot(ds_.TIME[cut], ds_.V[cut])
-axs[1].plot(ds_.TIME[cut], vl[cut])
-axs[2].plot(ds_.TIME[cut], ds_.U_ERR[cut], ".")
+axs[0].plot(ds_.time[cut], ds_.u[cut])
+axs[0].plot(ds_.time[cut], ul[cut])
+axs[1].plot(ds_.time[cut], ds_.v[cut])
+axs[1].plot(ds_.time[cut], vl[cut])
+axs[2].plot(ds_.time[cut], ds_.u_err[cut], ".")
 
 fig.autofmt_xdate()
+
+print(f"Days in chunk: {(i1 - i0)/24}")
+
+# %%
+nperseg = 256
+
+freq, Puu = sig.csd(ds_.u[i0:i1], ds_.u[i0:i1], fs=24, nperseg=nperseg)
+_, Pvv = sig.csd(ds_.v[i0:i1], ds_.v[i0:i1], fs=24, nperseg=nperseg)
+_, Puv = sig.csd(ds_.u[i0:i1], ds_.v[i0:i1], fs=24, nperseg=nperseg)
+freq2, Puu2 = sig.csd(ds_.u[i0:i1], ds_.u[i0:i1], fs=24, nperseg=nperseg/2)
+_, Pvv2 = sig.csd(ds_.v[i0:i1], ds_.v[i0:i1], fs=24, nperseg=nperseg/2)
+_, Puv2 = sig.csd(ds_.u[i0:i1], ds_.v[i0:i1], fs=24, nperseg=nperseg/2)
+
+fig, axs = plt.subplots(2, 1, sharex=True, figsize=(5, 10))
+axs[0].loglog(freq, Puu.real, "C0:")
+axs[0].loglog(freq, Pvv.real, "C1:")
+axs[0].loglog(freq2, Puu2.real, "C0")
+axs[0].loglog(freq2, Pvv2.real, "C1")
+axs[1].semilogx(freq, Puv.real, "C0:")
+axs[1].semilogx(freq2, Puv2.real, "C0")
+
+f_mean = gsw.f(ds_.lat[i0:i1].mean())*86400/(np.pi*2)
+
+for ax in axs:
+    ax.axvline(f_mean, color="k")
+    ax.axvline(1/T_M2, color="gray")
 
 # %%
